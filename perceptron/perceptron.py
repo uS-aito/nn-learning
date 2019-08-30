bda43315dd15d5cd8b63004c5e2c3de44faf2627
yusaito04 <yu.saito04@g.softbank.co.jp>
2019-08-30 17:12:46 +0900

    デバッグ

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -15,7 +15,7 @@ class Perceptron(object):
         for _ in range(self.n_iter):
             errors = 0
             for xi, target in zip(X,y):
-                update = self.eta * (target - self.predict(X))
+                update = self.eta * (target - self.predict(xi))
                 self.w_[1:] += update * xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
@@ -40,17 +40,17 @@ import numpy as np
 y = df.iloc[0:100, 4].values
 y = np.where(y == "Iris-setosa", -1, 1)
 X = df.iloc[0:100, [0,2]].values
-plt.scatter(X[:50, 0], X[:50, 1], color="red", marker="o", label="setosa")
-plt.scatter(X[50:100, 0], X[50:100, 1], color="blue", marker="x", label="versicolor")
-plt.xlabel("sepal length[cm]")
-plt.ylabel("petal length[cm]")
-plt.legend(loc="upper left")
-plt.show()
+# plt.scatter(X[:50, 0], X[:50, 1], color="red", marker="o", label="setosa")
+# plt.scatter(X[50:100, 0], X[50:100, 1], color="blue", marker="x", label="versicolor")
+# plt.xlabel("sepal length[cm]")
+# plt.ylabel("petal length[cm]")
+# plt.legend(loc="upper left")
+# plt.show()
 
 # 学習
 ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X,y)
-plt.plot(range(1,len(ppn.errors_)), ppn.errors_, marker="o")
+plt.plot(range(1,len(ppn.errors_) + 1), ppn.errors_, marker="o")
 plt.xlabel("Epochs")
 plt.ylabel("Number of update")
 plt.show()
